f414810e9d85374160c6cc20db0e338a25f20693
htpcfreak <htpcfreak@c424e040-ec7e-11de-a700-c3e9af1e83fa>
2010-05-27 19:10:53 +0000

    Version 1.2  fixed: Torec is back with us  change: Some improvements for logging  change: Now try to rely more on local filename parsing and only then on google/imdb names. usually I parse the file name  		 into movie name quite decently, so try to use that first.  change: do NOT get the movie pic automatically

--- Subs4me/src/subs/Subs4me.java
+++ Subs4me/src/subs/Subs4me.java
@@ -19,7 +19,7 @@ import utils.Utils;
 public class Subs4me
 {
     public static final String SRT_EXISTS = "/c";
-    public static final String VERSION = "1.1.3";
+    public static final String VERSION = "1.2";
     public static final String SUBS4ME_IGNORE_FILE_NAME = "ignore_directory.subs4me";
     
     public static final String RECURSIVE_SEARCH = "/r";
